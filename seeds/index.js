ebe310ff5ae3e813de91c796745f7a00a4c87163
Edgar Acuna <acunamatata.ea@gmail.com>
2022-02-16 14:27:32 -0500

    added maps using mapbox

--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,7 @@ const seedDB = async () => {
       title: `${sample(descriptors)} ${sample(places)}`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       price,
+      geometry: { type: 'Point', coordinates: [-113.1331, 47.0202] },
       images: [
         {
           url: 'https://res.cloudinary.com/dvp6ieed5/image/upload/v1644963647/YelpCamp/wrdiiujhknbv1bsts6hg.jpg',
