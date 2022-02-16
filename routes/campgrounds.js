8e245cdb19d4f63f7f5cf23892e319a028161a7d
Edgar Acuna <acunamatata.ea@gmail.com>
2022-02-15 23:08:58 -0500

    added auth, stars rating feature and uploading pictures functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,67 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync');
 const { campgroundSchema } = require('../schemas.js')
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const { isLoggedIn } = require('../middleware')
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const { post } = require('./users');
+const upload = multer({ storage });
 
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
-}
+router.route('/')
+  .get(catchAsync(campgrounds.index))
+  .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
-router.get('/', catchAsync(async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render('campgrounds/index', { campgrounds })
-}))
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/new', isLoggedIn, (req, res) => {
-  res.render('campgrounds/new');
-})
+router.route('/:id')
+  .get(catchAsync(campgrounds.showCampground))
+  .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  req.flash('success', 'Successfully made a new campground');
-  res.redirect(`/campgrounds/${campground._id}`)
-}))
 
-router.get('/:id', catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id).populate('reviews');
-  if (!campground) {
-    req.flash('error', 'Cannot find that campground');
-    return res.redirect('/campgrounds')
-  }
-  res.render('campgrounds/show', { campground });
-}))
-
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  if (!campground) {
-    req.flash('error', 'Cannot find that campground');
-    return res.redirect('/campgrounds')
-  }
-  res.render('campgrounds/edit', { campground });
-}))
-
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-  req.flash('success', 'successfully updated campground!')
-  res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-router.delete('/:id', catchAsync(async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  req.flash('success', 'successfully deleted campground')
-  res.redirect('/campgrounds');
-}))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -9,28 +9,38 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            price
-        })
-        await camp.save();
-    }
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: '620a3bcefad6938fc99743f9',
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dvp6ieed5/image/upload/v1644963647/YelpCamp/wrdiiujhknbv1bsts6hg.jpg',
+          filename: 'YelpCamp/wrdiiujhknbv1bsts6hg',
+        },
+        {
+          url: 'https://res.cloudinary.com/dvp6ieed5/image/upload/v1644963647/YelpCamp/ik8ndhatmrf2nacuyszm.jpg',
+          filename: 'YelpCamp/ik8ndhatmrf2nacuyszm',
+        }
+      ]
+    })
+    await camp.save();
+  }
 }
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 })
